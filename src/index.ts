c26ee841ec16945d932c0341ba2111cffb19b70f
ss49919201 <sakashin10291029@gmail.com>
2024-08-25 14:38:39 +0900

    feat: convert file to base64

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-type FileToBase64 = (file: File) => Promise<string>;
+import { readFileSync } from "fs";
+
+type FileToBase64 = (filePath: string) => string;
 
 /**
- * !!!!!! Worning: This is a dummy implementation. !!!!!!
  * Converts a file to a base64 string.
  */
-export const fileToBase64: FileToBase64 = async (file: File) => {
-  return Promise.resolve("base64");
+export const fileToBase64: FileToBase64 = (filePath: string) => {
+  return readFileSync(filePath).toString("base64");
 };
